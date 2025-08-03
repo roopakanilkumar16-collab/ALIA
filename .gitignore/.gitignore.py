dee1ee5be3db3544c620a0bfb2973cad20d2bfbb
Roopak.A <roopakanilkumar16@gmail.com>
2025-08-03 12:07:12 +0530

    Add .gitignore file

---
+++ .gitignore/.gitignore.py
@@ -0,0 +1,27 @@
+# Byte-compiled / optimized / DLL files
+__pycache__/
+*.py[cod]
+*$py.class
+
+# C extensions
+*.so
+
+# Distribution / packaging
+.Python
+build/
+dist/
+eggs/
+*.egg-info/
+.env
+
+# Environments
+venv/
+env/
+.venv/
+
+# Project-specific files
+*.pkl
+.gitconfig
+
+# Log files
+*.log
